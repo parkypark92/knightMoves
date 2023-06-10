b1839a2ad21ce64ff43d53d0382155fa99b9165a
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-06-10 12:26:28 +0100

    create a chess board represented as an array of coordinates. Create a knight object. Make a function to determine the set of legal moves given a starting coordinate

---
+++ index.js
@@ -0,0 +1,31 @@
+const board = [];
+for (let i = 0; i <= 7; i++) {
+  for (let j = 0; j <= 7; j++) {
+    let square = [i, j];
+    board.push(square);
+  }
+}
+
+const knight = { coord: [] };
+
+function legalMoves(coord) {
+  let x = coord[0];
+  let y = coord[1];
+  let allMoves = [
+    [x + 1, y - 2],
+    [x + 2, y - 1],
+    [x + 2, y + 1],
+    [x + 1, y + 2],
+    [x - 1, y + 2],
+    [x - 2, y + 1],
+    [x - 2, y - 1],
+    [x - 1, y - 2],
+  ];
+  let legal = [];
+  for (let move of allMoves) {
+    if (move[0] >= 0 && move[1] >= 0 && move[0] <= 7 && move[1] <= 7) {
+      legal.push(move);
+    }
+  }
+  return legal;
+}
